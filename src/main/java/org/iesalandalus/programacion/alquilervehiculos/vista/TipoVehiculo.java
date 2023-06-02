e99c9a79efc447bac7273a14ed36df8f68ed1fe2
José <Jos@DESKTOP-J0SE>
2023-06-02 18:45:05 +0200

    He creado el enumerado TipoVehiculo

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/TipoVehiculo.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/TipoVehiculo.java
@@ -1,5 +1,55 @@
 package org.iesalandalus.programacion.alquilervehiculos.vista;
 
-public class TipoVehiculo {
+import java.org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Autobus;
+import java.org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Furgoneta;
+import java.org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Vehiculo;
 
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Turismo;
+
+public enum TipoVehiculo {
+
+	TURIMOS("Turismo"), AUTOBUS("Autobus"), FURGONETA("Furgoneta");
+
+	private String nombre;
+
+	private TipoVehiculo(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private static boolean esOrdinalValido(int ordinal) {
+		boolean aux = false;
+		if ((ordinal >= 0) && (ordinal <= Accion.values().length)) {
+
+			aux = true;
+		}
+		return aux;
+	}
+
+	public static TipoVehiculo get(int ordinal) {
+		if (!esOrdinalValido(ordinal)) {
+			throw new IllegalArgumentException("ERROR: El ordinal no es válido");
+		}
+
+		return TipoVehiculo.values()[ordinal];
+	}
+
+	public static TipoVehiculo get(Vehiculo vehiculo) {
+		TipoVehiculo tipoVehiculo = null;
+		if (vehiculo instanceof Turismo) {
+			tipoVehiculo = TURIMOS;
+		}
+		if (vehiculo instanceof Autobus) {
+			tipoVehiculo = AUTOBUS;
+		}
+		if (vehiculo instanceof Furgoneta) {
+			tipoVehiculo = FURGONETA;
+		}
+
+		return tipoVehiculo;
+	}
+
+	@Override
+	public String toString() {
+		return nombre;
+	}
 }
