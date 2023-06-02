ea72e918257e3acf34d8fa9cd1b2849f597ed9c4
José <Jos@DESKTOP-J0SE>
2023-06-02 18:50:14 +0200

    .

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/FactoriaVista.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/FactoriaVista.java
@@ -1,7 +1,5 @@
 package org.iesalandalus.programacion.alquilervehiculos.vista;
 
-import org.iesalandalus.programacion.alquilervehiculos.vista.texto.VistaTexto;
-
 public enum FactoriaVista {
 
 	TEXTO {
