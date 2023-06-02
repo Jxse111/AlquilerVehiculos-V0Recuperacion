fc9fe570e422f162fbd1e3d8303c5487a08a503b
José <Jos@DESKTOP-J0SE>
2023-06-02 18:51:50 +0200

    He ajustado MainApp

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -1,9 +1,21 @@
 package org.iesalandalus.programacion.alquilervehiculos;
 
+import java.org.iesalandalus.programacion.alquilervehiculos.modelo.FactoriaFuenteDatos;
+
+import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
+import org.iesalandalus.programacion.alquilervehiculos.vista.FactoriaVista;
+import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		
+		Vista vista = FactoriaVista.TEXTO.crear();
+		ModeloCascada modeloCascada = new ModeloCascada(FactoriaFuenteDatos.MEMORIA.crear());
+		Controlador controlador = new Controlador(modeloCascada, vista);
+
+		controlador.comenzar();
+		controlador.terminar();
+
 	}
 
 }
