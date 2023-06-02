11460c33111809578865c6d3540e9d87bd449e1b
José <Jos@DESKTOP-J0SE>
2023-06-02 18:41:00 +0200

    He actualizado consola

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/Consola.java
@@ -1,112 +1,170 @@
 package org.iesalandalus.programacion.alquilervehiculos.vista;
+
+import java.awt.Desktop.Action;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+
 import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Alquiler;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Turismo;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
+
 	private static final String PATRON_FECHA = "dd/MM/yyyy";
 	private static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern(PATRON_FECHA);
+	private static final String PATRON_MES = "MM/yyyy";
 
-	public static void mostrarCabecera(String mensaje) {
-		System.out.println(mensaje);
-		System.out.println("-".repeat(mensaje.length()));
+	private Consola() {
 
 	}
 
-	public static void mostrarMenu() {
-		mostrarCabecera("     MENÚ DE OPCIONES     ");
-
-		for (int i = 0; i < (Opcion.values().length); i++) {
-			System.out.println(Opcion.values()[i]);
+	public static void mostrarCabecera(String mensaje) {
+		StringBuilder cabecera = new StringBuilder();
+		cabecera.append("-");
+		System.out.printf("%n%s%n", mensaje);
+		for (int i = 0; i < mensaje.length(); i++) {
+			System.out.print(cabecera);
 		}
 		System.out.println("");
+	}
+
+	public static void mostrarMenuAcciones() {
+		mostrarCabecera("menú");
+		for (int i = 0; i < Action.values().length; i++) {
+			System.out.printf("%d.-%s %n", i, Action.values()[i]);
+		}
+	}
+
+	public static Action elegirAccion() {
+		int auxElegirAccion = 0;
+
+		do {
+			auxElegirAccion = leerEntero("Introduce una acción: ");
+		} while (auxElegirAccion < 0 && auxElegirAccion > Action.values().length);
+
+		return Action.values()[auxElegirAccion];
 	}
 
 	private static String leerCadena(String mensaje) {
 		System.out.print(mensaje);
 		return Entrada.cadena();
-
 	}
 
 	private static Integer leerEntero(String mensaje) {
-		System.out.printf("%s", mensaje);
+		System.out.print(mensaje);
 		return Entrada.entero();
-
 	}
 
-	private static LocalDate leerFecha(String mensaje) {
-		LocalDate fecha = null;
+	private static LocalDate leerFecha(String mensaje, String patron) {
+		LocalDate aux = null;
+
 		try {
-			String cadena = leerCadena(mensaje);
-			fecha = LocalDate.parse(cadena, FORMATO_FECHA);
-
-		} catch (DateTimeParseException e) {
-			System.out.print(e.getMessage());
-			fecha = null;
-		}
-		return fecha;
-	}
-
-	public static Opcion elegirOpcion() {
-		Opcion opcion = null;
-		do {
-			try {
-				int num = leerEntero("Selecciona una opción: ");
-
-				opcion = Opcion.get(num);
-			} catch (NullPointerException e) {
-				System.out.println(e.getMessage());
+			System.out.print(mensaje);
+			if (patron.equals(PATRON_MES)) {
+				aux = LocalDate.parse("01/" + Entrada.cadena(), FORMATO_FECHA);
+			} else {
+				aux = LocalDate.parse(Entrada.cadena(), FORMATO_FECHA);
 			}
-		} while (opcion == null);
-		return opcion;
+		} catch (DateTimeParseException e) {
+			System.out.println(e.getMessage());
+
+		}
+		return aux;
 	}
 
 	public static Cliente leerCliente() {
-
-		return new Cliente(leerCadena("Introduce el nombre del cliente: "),
-				leerCadena("Introduce el DNI del cliente: "), leerCadena("Introduce el teléfono del cliente: "));
+		Cliente nuevoCliente = null;
+		nuevoCliente = new Cliente(leerNombre(), leerCadena("Introduce un dni: "), leerTelefono());
+		return nuevoCliente;
 
 	}
 
 	public static Cliente leerClienteDni() {
-		return Cliente.getClienteConDni(leerCadena("Introduce el DNI del cliente: "));
+		return Cliente.getClienteConDni(leerCadena("Introduce un dni: "));
 
 	}
 
 	public static String leerNombre() {
-		return leerCadena("Introduce el nombre: ");
+
+		return leerCadena("Introduce un nombre: ");
 
 	}
 
 	public static String leerTelefono() {
-		return leerCadena("Introduce el teléfono: ");
+		return leerCadena("Introduce un número de teléfono: ");
 
 	}
 
-	public static Turismo leerTurismo() {
-		return new Turismo(leerCadena("Introduce la marca del turismo: "),
-				(leerCadena("Introduce el modelo del turismo: ")),
-				(leerEntero("Introduce la cilindrada del turismo: ")),
-				(leerCadena("Introduce la matrícula del turismo: ")));
+	public static Vehiculo leerVehiculo() {
+		mostrarMenuTiposVehiculos();
+		return leerVehiculo(elegirTipoVehiculo());
 
 	}
 
-	public static Turismo leerTurismoMatricula() {
-		return Turismo.getTurismoConMatricula(leerCadena("Introduce la matrícula del turismo: "));
+	private static void mostrarMenuTiposVehiculos() {
+		mostrarCabecera("menú tipo de vehículos; ");
+		for (int i = 0; i < TipoVehiculo.values().length; i++) {
+			System.out.printf("%d.-%s %n", i, TipoVehiculo.values()[i]);
+		}
+	}
+
+	private static TipoVehiculo elegirTipoVehiculo() {
+		TipoVehiculo auxElegirAccion = null;
+		do {
+			try {
+				auxElegirAccion = TipoVehiculo.get(leerEntero("Introduce una acción: "));
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		} while (auxElegirAccion == null);
+
+		return auxElegirAccion;
+	}
+
+	private static Vehiculo leerVehiculo(TipoVehiculo tipoVehiculo) {
+		Vehiculo vehiculo = null;
+		String marca = leerCadena("Introduce una marca: ");
+		String modelo = leerCadena("Introduce un modelo: ");
+		String matricula = leerCadena("Introduce una matrícula: ");
+
+		if (tipoVehiculo == TipoVehiculo.TURIMOS) {
+			vehiculo = new Turismo(marca, modelo, leerEntero("Introduce una cilindrada: "), matricula);
+		}
+		if (tipoVehiculo == TipoVehiculo.AUTOBUS) {
+			vehiculo = new Autobus(marca, modelo, leerEntero("Introduce un número de plazas: "), matricula);
+		}
+		if (tipoVehiculo == TipoVehiculo.FURGONETA) {
+
+			vehiculo = new Furgoneta(marca, modelo, leerEntero("Introduce el pma: "),
+					leerEntero("Introduce un número de plazas: "), matricula);
+		}
+
+		return vehiculo;
+	}
+
+	public static Vehiculo leerVehiculoMatricula() {
+		return Vehiculo.getVehiculoConMatricula(leerCadena("Introduce una matricula: "));
 
 	}
 
 	public static Alquiler leerAlquiler() {
-		return new Alquiler(leerClienteDni(), leerTurismoMatricula(), leerFecha("Introduce la fecha del alquiler: "));
+		Alquiler nuevoAlquiler = null;
+		nuevoAlquiler = new Alquiler(leerClienteDni(), leerVehiculoMatricula(),
+				leerFecha("Introduce una fecha de alquiler: ", PATRON_FECHA));
+		return nuevoAlquiler;
 
 	}
 
 	public static LocalDate leerFechaDevolucion() {
-		return leerFecha("Introduce la fecha de devolución: ");
+		return leerFecha("Introduce una fecha de devolución: ", PATRON_FECHA);
 
 	}
+
+	public static LocalDate leerMes() {
+		return leerFecha("Introduceme un mes y un año: ", PATRON_MES);
+	}
+
 }
